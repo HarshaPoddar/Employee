70eed03095d4d567596645b3724d03616c66d05f
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-28 09:50:20 +0530

    Complete employee assignment!

---
+++ Employee LinQ/Employee LinQ/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_LinQ
+{
+
+
+    class Employee
+    {
+        public int EmployeeID;
+        public int Salary;
+        public int Department;
+
+        public Employee(int ID, int salary, int department)
+        {
+            EmployeeID = ID;
+            Salary = salary;
+            Department = department;
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<Employee> employee = new List<Employee>(5);
+
+            employee.Add(new Employee(1, 30000, 2));
+            employee.Add(new Employee(2, 20000, 1));
+            employee.Add(new Employee(3, 5000, 3));
+            employee.Add(new Employee(4, 30000, 2));
+            employee.Add(new Employee(5, 4000, 3));
+
+            var Result = from value in employee
+                         where value.Salary >= 10000
+                         group value by value.Department into EmployeeGroup
+                         select new
+                         {
+                             count = EmployeeGroup.Count(),
+                             key = EmployeeGroup.Key
+                         };
+
+
+
+            foreach (var Answer in Result)
+                Console.WriteLine(Answer);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Employee LinQ/Employee LinQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Employee LinQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Neudesic")]
+[assembly: AssemblyProduct("Employee LinQ")]
+[assembly: AssemblyCopyright("Copyright © Neudesic 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98f4e64b-6614-4342-a7c1-970f062f6179")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
